ae635220f62476623741acccf26b7e3ef144eaf1
yohan oh <yohan.oh332@gmail.com>
2021-03-29 12:08:19 -0400

    fixed some errors

--- frontend/src/util/route_util.js
+++ frontend/src/util/route_util.js
@@ -1,23 +1,4 @@
 import React from 'react';
-import { Provider } from 'react-redux';
-import { HashRouter } from 'react-router-dom';
-// We'll create this soon
-import App from './app';
-
-const Root = ({ store }) => (
-  <Provider store={ store }>
-    <HashRouter>
-      <App />
-    </HashRouter>
-  </Provider>
-);
-
-export default Root;
-
-
-// src/util/route_util.js
-
-import React from 'react';
 import { connect } from 'react-redux';
 import { Route, Redirect, withRouter } from 'react-router-dom';
 
